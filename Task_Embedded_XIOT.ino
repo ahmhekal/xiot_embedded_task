ed7eb4157266ffd18ba5808f17dc5210ac953dd8
Ahmed Hekal <ahmhekal@gmail.com>
2018-06-30 23:31:10 +0200

    Add files via upload

---
+++ Task_Embedded_XIOT.ino
@@ -0,0 +1,32 @@
+int led = 1; //digital pin 1
+int interrupt = 2; //digital pin 2 for the switch
+volatile byte state = LOW; //initial state for the led
+int tempSensor = A1; //analog pin 1
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(tempSensor, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(interrupt, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interrupt), func, CHANGE); //if the switch is pressed (interrupt), execute : func
+}
+
+void loop()
+{
+  float tempValue = float( analogRead(tempSensor) ) / 1023; 
+  tempValue = tempValue * 500;
+  Serial.println(tempValue);
+  delay(3000); //three seconds
+}
+
+void func() 
+{
+  state = !state; //toggle the state of the led whenever the switch is pressed
+  digitalWrite(led, state); //apply the state to the led
+  Serial.print("pressed") ; 
+  if(state==HIGH)
+    Serial.println("ON");
+  else
+    Serial.println("OFF");
+}
